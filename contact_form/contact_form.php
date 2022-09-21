592d5928fa1a14ef1e6d18b4291c15c074546d7f
Jesus Martheyn <jesusmartheyn@gmail.com>
2022-09-20 21:05:56 -0500

    php

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -1,7 +1,7 @@
 <?php
 
 // configure
-$from = 'info@yourdomain.com'; // Replace it with Your Hosting Admin email. REQUIRED!
+$from = 'jesusmartheyn.github.io'; // Replace it with Your Hosting Admin email. REQUIRED!
 $sendTo = 'jesusmartheyn@mail.com'; // Replace it with Your email. REQUIRED!
 $subject = 'New message from jesusmartheyn.com contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message'); // array variable name => Text to appear in the email. If you added or deleted a field in the contact form, edit this array.
